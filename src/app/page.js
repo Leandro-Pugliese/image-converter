e658adf1d42e69d63c4d70f5bc2b0cbc9cce1442
Leandro-Pugliese <lean.pugliese@gmail.com>
2024-11-20 13:43:26 -0300

    Conversor imagen listo

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,128 @@
-import Image from "next/image";
+'use client'
+import { useState } from "react";
+import axios from '@/app/utils/axios'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-export default function Home() {
+export default function ImageUpload() {
+  const [image, setImage] = useState(null);
+  const [changeFile, setChangeFile] = useState('');
+  const [selectedFileName, setSelectedFileName] = useState('');
+  const [format, setFormat] = useState("jpeg");
+  const [convertedImage, setConvertedImage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  //manejo el cambio en el input de la imagen
+  const handleImageChange = (e) => {
+    e.preventDefault();
+    const file = e.target.files[0];
+    setImage(file);
+    if (file) {
+      setChangeFile("Cambiar Imagen");
+      setSelectedFileName(file.name);
+    }
+  };
+  //manejo el cambio en el input del formato
+  const handleFormatChange = (e) => {
+    setFormat(e.target.value);
+  };
+
+  //manejo el envío del formulario
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!image) {
+      return alert("Por favor, selecciona una imagen.");
+    }
+    const formData = new FormData();
+    formData.append("imagen", image);
+    formData.append("format", format);
+
+    try {
+      setLoading(true);
+      setError('');
+      const config = {
+        method: "post",
+        url: `/convertir`,
+        data: formData,
+        headers: {
+          "Content-Type": "multipart/form-data"
+        },
+        responseType: "arraybuffer", //Para recibir la imagen en formato binario
+      };
+      const response = await axios(config);
+      const blob = new Blob([response.data], {type: `image/${format}`});
+      const imageUrl = URL.createObjectURL(blob);
+      setConvertedImage(imageUrl);
+      setLoading(false);
+    } catch (err) {
+      setError("Error al convertir la imagen");
+      setLoading(false);
+    }
+  };
+
+  //Descargar la imagen convertida
+  const handleDownload = () => {
+    if (convertedImage) {
+      const a = document.createElement("a");
+      a.href = convertedImage;
+      a.download = `${selectedFileName}-converted.${format}`;
+      a.click();
+    }
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="bg-gray-800 min-h-screen flex items-center flex-col text-white">
+      <h1 className="mt-4">Conversor de Imágenes</h1>
+      <form onSubmit={handleSubmit} className="pt-8 w-full text-center mb-4">
+        <div className="flex flex-col items-center">
+          <label 
+            htmlFor="image"
+            className="cursor-pointer bg-pink-800 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            {changeFile || 'Seleccionar Imagen'}
+          </label>
+          <input 
+            type="file" 
+            id="image" 
+            onChange={handleImageChange} 
+            className="hidden"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {
+          (selectedFileName) &&
+          <div className='mt-2'>
+            <p className='ml-2'>{selectedFileName}</p>
+          </div>
+        }
+        <div className="mt-6">
+          <label htmlFor="format" className="mr-4">Formato de destino:</label>
+          <select id="format" value={format} onChange={handleFormatChange} className="bg-pink-800 outline-none cursor-pointer rounded">
+            <option value="jpeg">JPEG</option>
+            <option value="png">PNG</option>
+            <option value="webp">WEBP</option>
+            <option value="gif">GIF</option>
+            <option value="tiff">TIFF</option>
+          </select>
+        </div>
+        <button type="submit" disabled={loading} className="bg-violet-800 text-white py-2 px-4 w-72 rounded cursor-pointer hover:bg-violet-700 mt-4">
+          {loading ? "Convirtiendo..." : "Convertir Imagen"}
+        </button>
+      </form>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {convertedImage && (
+        <div>
+          <h2>Imagen Convertida:</h2>
+          <img src={convertedImage} alt="Imagen Convertida" />
+          <button 
+            className="bg-violet-800 text-white py-2 px-4 w-full rounded cursor-pointer hover:bg-violet-700 mt-2 mb-4" 
+            onClick={handleDownload}
+          >
+            Descargar imagen
+            <i className="bi bi-download mx-2"/>
+          </button>
+        </div>
+      )}
     </div>
   );
 }
+
